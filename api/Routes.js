584795da263ea65c39133242dfdaab4373ba013b
harshv0624 <harshvonmail@gmail.com>
2024-06-11 11:51:07 +0530

    Basics 2

--- api/Routes.js
+++ api/Routes.js
@@ -1,9 +1,56 @@
 const app=require('express')
+const crypto=require("crypto")
+const User=require("./models/user")
+const nodemailer=require('nodemailer')
+require("dotenv").config()
 
 const router=app.Router()
 
-router.post("/",(req,res)=>{
-    console.log("Done")
-    res.send("Ok")
+const sendVerificationEmail=async(email,token)=>{
+    const transporter=nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:process.env.USER,
+            pass:process.env.PASS
+        }
+    })
+
+    const mailOption={
+        from:'matchmake.com',
+        to:email,
+        subject:"Email Verification",
+        text:`Please click on the following link to verify your email: http://localhost:3000/verify/${token}`
+    }
+
+    try{
+        await transporter.sendMail(mailOption)
+
+    }catch(error){
+        console.log("Error while sending email")
+    }
+}
+
+router.post("/register",async(req,res)=>{
+    try{
+        const {name,email,password}=req.body
+        const existingUser=await User.findOne({email})
+        if(existingUser){
+            res.status(400).json({message:"User already exist"})
+        }
+
+        const user=new User({
+            name,
+            email,
+            password
+        })
+
+        user.verificationToken=crypto.randomBytes(20).toString("hex")
+        await user.save()
+
+        sendVerificationEmail(user.email,user.verificationToken)
+    }catch(error){
+        console.log("Error while registering")
+        res.status(500).json({message:"Registration Failed"})
+    }
 })
 module.exports=router
